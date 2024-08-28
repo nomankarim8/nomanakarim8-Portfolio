82cac98cacb852d23eb8b0c704177f3a153b93ad
nomankarim8 <karimnoman97@gmail.com>
2024-08-28 07:01:32 -0700

    update appp.js

--- appp.js
+++ appp.js
@@ -37,7 +37,6 @@ const header = document.querySelector('.header.container');
 
 
 
-
 hamburger.addEventListener('click', () => {
 	hamburger.classList.toggle('active');
 	mobile_menu.classList.toggle('active');
