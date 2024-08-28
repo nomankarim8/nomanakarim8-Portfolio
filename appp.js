eead9f656cc9a2c9905887b39f53633b701a5238
nomankarim8 <karimnoman97@gmail.com>
2024-08-28 07:01:46 -0700

    update appp.js

--- appp.js
+++ appp.js
@@ -4,39 +4,6 @@ const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a'
 const header = document.querySelector('.header.container');
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 hamburger.addEventListener('click', () => {
 	hamburger.classList.toggle('active');
 	mobile_menu.classList.toggle('active');
